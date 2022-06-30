47311186f5f3f18dd841b33b3fddb1b914c1f258
valentina-16maximova <valentina-16-maks@mail.ru>
2022-07-01 00:10:44 +0300

    Modul 12

---
+++ script.py
@@ -0,0 +1,6 @@
+money = 100000
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = [0.056 * money, 0.059 * money, 0.0428 * money, 0.040 * money]
+list_deposit = deposit
+print(list_deposit)
+print(max(deposit))
